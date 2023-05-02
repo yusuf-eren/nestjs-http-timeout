788dee5c20716b60cec1ae0b8a96e4b009f76296
erenyusuf <yusuf.eren@getir.com>
2023-05-02 14:53:25 +0300

    Revert "feat: readme file"
    This reverts commit c922172ff60cd33de8360ec9ea2054a4d6e6f034.

--- timeout.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { UseInterceptors, applyDecorators } from '@nestjs/common';
-import { TimeoutInterceptor } from './timeout.interceptor';
-
-export function RequestTimeout(timeoutInMs: number) {
-  return applyDecorators(UseInterceptors(new TimeoutInterceptor(timeoutInMs)));
-}
