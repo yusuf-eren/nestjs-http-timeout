c922172ff60cd33de8360ec9ea2054a4d6e6f034
erenyusuf <yusuf.eren@getir.com>
2023-05-02 14:53:16 +0300

    feat: readme file

---
+++ timeout.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  RequestTimeoutException,
+} from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  constructor(private readonly timeoutInMs: number) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<RequestTimeoutException | Error> {
+    return next.handle().pipe(
+      timeout(this.timeoutInMs),
+      catchError((error) => {
+        if (error instanceof TimeoutError) {
+          return throwError(() => new RequestTimeoutException());
+        }
+        return throwError(() => error);
+      }),
+    );
+  }
+}
